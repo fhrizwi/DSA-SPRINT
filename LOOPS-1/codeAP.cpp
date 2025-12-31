d708b3f1d99e0b4e2b69dadfcdf146784539a6d6
fhrizwi <faizulhaque2002@gmail.com>
2026-01-01 00:37:30 +0530

    added

---
+++ LOOPS-1/codeAP.cpp
@@ -0,0 +1,33 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cout << "Enter number of terms of Arithmetic progression: ";
+//     cin >> n;
+//     for (int i = 5; i <= 3 * n + 2; i += 3)
+//     {
+//         cout << i << endl;
+//     }
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int term = 5;   // first term
+
+    cout << "Enter number of terms of Arithmetic Progression: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << term << endl;
+        term += 3;   // common difference
+    }
+
+    return 0;
+}
