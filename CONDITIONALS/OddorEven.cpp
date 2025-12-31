1dd6152ecf4936a1f38332df179bf97989210423
fhrizwi <faizulhaque2002@gmail.com>
2025-12-31 21:28:14 +0530

    added

---
+++ CONDITIONALS/OddorEven.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number:";
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << n << " is an Even Number"  << endl;
+    }
+    else
+    {
+        cout << n<<" is an Odd Number" ;
+    }
+    return 0;
+}
