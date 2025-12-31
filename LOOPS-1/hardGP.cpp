842bc606886c8edb35090c0d101ea783b7cedc1d
fhrizwi <faizulhaque2002@gmail.com>
2026-01-01 00:57:14 +0530

    added

---
+++ LOOPS-1/hardGP.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 100;
+
+    for (int i = 1; i <= 34; i++)
+    {
+        cout << a << endl;
+        a = a - 3; // common difference
+    }
+
+    return 0;
+}
