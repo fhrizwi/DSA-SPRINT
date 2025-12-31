b48049027b2d528f55be1502df6fb2455ac0b232
fhrizwi <faizulhaque2002@gmail.com>
2025-12-31 23:53:15 +0530

    added

---
+++ LOOPS-1/BasicsOfLoops.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << "Happy New Year 2026" << endl;
+    }
+    return 0;
+}
---
+++ LOOPS-1/ntimes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter n times";
+    for (int i = 0; i <= n; i++)
+    {
+        cout << "Happy New Year 2026" << endl;
+    }
+    return 0;
+}
