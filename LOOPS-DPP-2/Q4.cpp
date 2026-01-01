e76f211cd1058146824a2e46bbe1c14235619270
fhrizwi <faizulhaque2002@gmail.com>
2026-01-02 00:17:24 +0530

    added

---
+++ LOOPS-DPP-2/Q4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 2; i++)
+    {
+        for (int j = i; j <= 2; j++)
+            cout << i << @;   //"@"
+    }
+}
+
+//This code will NOT compile ❌ because of a syntax error.
+
+//❌ Error
