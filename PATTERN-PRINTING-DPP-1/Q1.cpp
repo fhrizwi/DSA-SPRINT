017989c6bc707d99ead264fd0aa62741b57d72ab
fhrizwi <faizulhaque2002@gmail.com>
2026-01-03 00:28:10 +0530

    added

---
+++ PATTERN-PRINTING-DPP-1/Q1.cpp
@@ -0,0 +1,24 @@
+// Print the following pattern
+// 1 1 1 1
+// 2 2 2 2
+// 3 3 3 3
+// 4 4 4 4
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 4;   // number of rows and columns
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
