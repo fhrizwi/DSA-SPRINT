281eab41d11740bf0b85a4d309a8f37193b79906
fhrizwi <faizulhaque2002@gmail.com>
2026-01-03 00:06:49 +0530

    added

---
+++ PATTERN-PRINTING-1/numberTriangle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// Enter number of rows: 4
+// 1
+// 12
+// 123
+// 1234
