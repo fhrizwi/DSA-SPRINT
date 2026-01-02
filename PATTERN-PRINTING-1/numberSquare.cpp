d3ee7ba093030b157ea15150d237ed39b1ca7f73
fhrizwi <faizulhaque2002@gmail.com>
2026-01-02 23:41:30 +0530

    added

---
+++ PATTERN-PRINTING-1/numberSquare.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of square: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+// Enter the size of square: 4
+// 1234
+// 1234
+// 1234
+// 1234
