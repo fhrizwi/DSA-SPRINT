311bb059dc006c617ab732429460e296ee2b07a5
fhrizwi <faizulhaque2002@gmail.com>
2026-01-04 23:30:15 +0530

    added

---
+++ FUNCTIONS/addressOfVariable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void function(int x, int y)
+{
+    cout << "Address of x in function variable" << &x << endl;
+    cout << "Address of x in function variable" << &y << endl;
+}
+int main()
+{
+    int x;
+    int y;
+    cout << "Address of x in main function variable" << &x << endl;
+    cout << "Address of x in main function variable" << &x << endl;
+    function(x, y);
+}
