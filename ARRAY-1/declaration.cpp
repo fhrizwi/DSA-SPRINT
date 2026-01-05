d7c51c185ee02f49002765cf7b75f4408fdd3222
fhrizwi <faizulhaque2002@gmail.com>
2026-01-06 00:35:17 +0530

    addded

---
+++ ARRAY-1/declaration.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //int arr[4]={1,5,6,9};
+    //char ch[100];
+    //bool flag[5];
+    //long arr[6];
+
+    // int arr[5];
+    // arr[0]=1;
+    // arr[0]=1;
+    // arr[0]=1;
+    // arr[0]=1;
+    // arr[0]=1;
+
+    //int arr[4]={1,2,3,4};
+    int arr[4]={1,5,6,9};
+    
+}
