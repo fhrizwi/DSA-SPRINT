fc708a7e75925cad7743bc89c9af6dfc0367d514
fhrizwi <faizulhaque2002@gmail.com>
2026-01-08 21:29:21 +0530

    added

--- ARRAY-1/declaration.cpp
+++ ARRAY-1/declaration.cpp
@@ -1,19 +1,24 @@
 #include<iostream>
 using namespace std;
 int main(){
-    //int arr[4]={1,5,6,9};
+    //int arr[4]={1,5,6,9}; //ye hai original syntax
+    //datatype variablename[arraysize]={elements}
+
     //char ch[100];
     //bool flag[5];
     //long arr[6];
 
-    // int arr[5];
-    // arr[0]=1;
-    // arr[0]=1;
-    // arr[0]=1;
-    // arr[0]=1;
-    // arr[0]=1;
+    // int arr[5];      //yaha pe size define kr diya
+    // arr[0]=1;        //yaha pe element store kr diya indexing krke
+    // arr[1]=1;
+    // arr[2]=1;
+    // arr[3]=1;
+    // arr[4]=1;
 
     //int arr[4]={1,2,3,4};
     int arr[4]={1,5,6,9};
     
+
+    int arr[]={1,2,3,4};  //bina size likhe bhi humm ek line me aese element likh sakte hai 
+
 }
