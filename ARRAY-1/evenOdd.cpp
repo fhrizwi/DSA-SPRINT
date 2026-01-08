f38b5565d23783c66fddc5c6276287aa299543ec
fhrizwi <faizulhaque2002@gmail.com>
2026-01-08 22:16:21 +0530

    added

---
+++ ARRAY-1/evenOdd.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the size of array";
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int evenCount = 0;
+    int oddCount = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            evenCount++;
+        }
+        else
+        {
+            oddCount++;
+        }
+    }
+    cout << "Even count is " << evenCount << endl;
+    cout << "Odd count is " << oddCount << endl;
+}
