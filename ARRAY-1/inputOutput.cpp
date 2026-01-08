53ee6312c642d8ddccd230bef862c9b35347c828
fhrizwi <faizulhaque2002@gmail.com>
2026-01-08 21:39:16 +0530

    added

--- ARRAY-1/inputOutput.cpp
+++ ARRAY-1/inputOutput.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
     int arr[5] = {1, 5, 6, 8, 9};
+    //cout<<arr[1]<<endl;     //agar mujhe sirf kisi specific index ka value chahiye tab. ye use kr sakte hai
     for (int i = 0; i < 5; i++)
     {
         cout << arr[i] << endl;
