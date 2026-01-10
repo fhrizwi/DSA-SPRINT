bca0c22f0c6fc9d8e452b46e31409a8eefac5a9c
fhrizwi <faizulhaque2002@gmail.com>
2026-01-11 01:38:35 +0530

    added

---
+++ VECTOR/vectorWithSize.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){             
+    //vector <int> v(3);     //agar sirf ek digit ho to wo size(no. of elements) hai 
+    vector <int> v(3,6);   //3 ->size  //6 -->element name 
+
+    cout<<v[0]<<endl;
+    cout<<v[1]<<endl;
+
+}
