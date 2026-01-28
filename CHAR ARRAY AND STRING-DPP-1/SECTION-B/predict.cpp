d0246816ada1c3ed9f0400965e00e78f9a2e8b68
fhrizwi <faizulhaque2002@gmail.com>
2026-01-28 23:08:20 +0530

    Add missing files; clean up old moved paths

---
+++ CHAR ARRAY AND STRING-DPP-1/SECTION-B/predict.cpp
@@ -0,0 +1,31 @@
+//PREDICT OUTPUT
+
+/* 
+1.
+char arr[] = {'H','e','l','l','o','\0'};
+int i = 0;
+while(arr[i] != '\0'){
+    cout << arr[i];
+    i++;
+}
+Output: Hello
+
+
+2.
+char arr[20] = "Alpha Intern";
+cout << arr[5];
+Output: (space)
+
+
+3.
+char arr[] = "DSA";
+for(int i = 0; arr[i] != '\0'; i++){
+    cout << i;
+}
+Output: 012
+
+
+
+
+
+*/
