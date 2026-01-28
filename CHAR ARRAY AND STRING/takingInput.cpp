cc3322000fc52ee1724e03c567d63537a5756aea
fhrizwi <faizulhaque2002@gmail.com>
2026-01-28 22:00:05 +0530

    added

--- CHAR ARRAY AND STRING/takingInput.cpp
+++ CHAR ARRAY AND STRING/takingInput.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 int main(){
     char ch [10];
-    cin>>ch;
-    cout<<ch;
+    cout<<"Enter the character: ";
+    // cin>>ch;       //ye sahi output print nahi dega
+
+    cin.getline(ch,10);        //this is right way
+    for (int i=0;i<10; i++){
+        cout<<ch[i]<<endl;
+    }
 
 }
+
+//sahi output print karne ke liye 
+//cin.getline   ka use karna hoga
+
+//aur jab hum input lenge to usme space ka use nhi karenge warrna ye space ko bhi count karega
