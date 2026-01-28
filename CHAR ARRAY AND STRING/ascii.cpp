0c45c60d93283e86bc26a766916dfe3ea1753092
fhrizwi <faizulhaque2002@gmail.com>
2026-01-28 21:27:46 +0530

    addded

---
+++ CHAR ARRAY AND STRING/ascii.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // char ch='@';
+    // cout<<(int)ch;
+
+    char ch='\0';
+    cout<<(int)ch<<endl;
+
+    char chh[5]={'a','b','c','d','e'};
+    cout<<(int)(chh[0]);
+}
