bc31ea857fbb9ffe80e025cd05262c70b92b15a3
fhrizwi <faizulhaque2002@gmail.com>
2026-01-31 20:41:41 +0530

    addedd

--- CHAR ARRAY AND STRING-DPP-1/SECTION-D/Q3.cpp
+++ CHAR ARRAY AND STRING-DPP-1/SECTION-D/Q3.cpp
@@ -1,3 +1,41 @@
 /*
 Input a string of even length and Reverse the first half of the string.
 */
+
+#include<iostream>
+using namespace std;
+int main(){
+    char ch[100];
+    cout<<"Input a string of even length to reverse first half of the string: ";
+    cin.getline(ch, 100);
+
+    int len = 0;
+
+    // Find length
+    while(ch[len] != '\0'){
+        len++;
+    }
+
+    // Check even length
+    if(len % 2 != 0){
+        cout << "String length is not even";
+        return 0;
+    }
+
+    int start = 0;
+    int end = (len / 2) - 1;
+
+    // Reverse first half
+    while(start < end){
+        char temp = ch[start];
+        ch[start] = ch[end];
+        ch[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    cout << "Result: " << ch;
+
+    return 0;
+}
