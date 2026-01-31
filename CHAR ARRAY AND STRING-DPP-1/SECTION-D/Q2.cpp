656eab0c4238b35b3216b2e89293600636273c5a
fhrizwi <faizulhaque2002@gmail.com>
2026-01-31 20:35:16 +0530

    addedd

--- CHAR ARRAY AND STRING-DPP-1/SECTION-D/Q2.cpp
+++ CHAR ARRAY AND STRING-DPP-1/SECTION-D/Q2.cpp
@@ -3,3 +3,21 @@
     - Input: `Alpha Intern`
     - Output: `Alpha@Intern`
 */
+
+#include<iostream>
+using namespace std;
+int main(){
+    char ch[100];
+    cout<<"Enter words with spaces like-Faizul Haque: ";
+    cin.getline(ch, 100);
+
+    for(int i = 0; ch[i] != '\0'; i++){
+        if(ch[i] == ' '){
+            ch[i] = '@';
+        }
+    }
+
+    cout << "After replacing spaces: " << ch;
+
+    return 0;
+}
